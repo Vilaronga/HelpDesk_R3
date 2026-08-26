9e4995a30053abb6e6af1636df4339123d0cd9c4
Lucas Vilaronga <lucasvilaronga@gmail.com>
2026-08-25 21:25:20 -0300

    Commit inicial

---
+++ HelpDesk.Api/Program.cs
@@ -0,0 +1,43 @@
+using HelpDesk.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using dotenv.net;
+
+//Carregar o .env e
+DotEnv.Load();
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Carrega as variáveis do banco
+var host = Environment.GetEnvironmentVariable("DB_HOST");
+var port = Environment.GetEnvironmentVariable("DB_PORT");
+var database = Environment.GetEnvironmentVariable("DB_DATABASE");
+var username = Environment.GetEnvironmentVariable("DB_USERNAME");
+var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+var connectionString = $"Host={host};Port={port};Database={database};Username={username};Password={password}";
+
+// Add services to the container.
+
+//Adiciona o contexto da db
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
